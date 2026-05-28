76af029334753fb3a500c5cab52a01fd7c6d398e
Rafael Stauffer <rs@opc6.net>
2026-05-28 20:04:55 +0200

    chore: set version string
    Signed-off-by: Rafael Stauffer <rs@opc6.net>

--- plugin.cs
+++ plugin.cs
@@ -18,7 +18,7 @@ namespace AlteredDestination
         public bool triedTargettingUnit;
     }
 
-    [BepInPlugin("com.checkpointcharlie.cruisemissile", "Checkpoint Charlie's Cruise Missile (Alternate destination)", "1.4.1")]
+    [BepInPlugin("com.checkpointcharlie.cruisemissile", "Checkpoint Charlie's Cruise Missile (Alternate destination)", "1.2.1")]
     public class AlteredDestinationPlugin : BaseUnityPlugin
     {
         public static ConditionalWeakTable<Missile, OverrideData> MissileWaypoints = new ConditionalWeakTable<Missile, OverrideData>();
