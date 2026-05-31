a82885cb209bd86ad4224e9d837834f50559c4bc
Rafael Stauffer <rs@opc6.net>
2026-05-31 19:59:59 +0200

    bump version to v1.4.1
    Signed-off-by: Rafael Stauffer <rs@opc6.net>

--- plugin.cs
+++ plugin.cs
@@ -19,7 +19,7 @@ namespace AlteredDestination
         public bool triedTargettingUnit;
     }
 
-    [BepInPlugin("com.checkpointcharlie.cruisemissile", "Checkpoint Charlie's Cruise Missile (Alternate destination)", "1.4.0")]
+    [BepInPlugin("com.checkpointcharlie.cruisemissile", "Checkpoint Charlie's Cruise Missile (Alternate destination)", "1.4.1")]
     public class AlteredDestinationPlugin : BaseUnityPlugin
     {
         public static ConditionalWeakTable<Missile, OverrideData> MissileWaypoints = new ConditionalWeakTable<Missile, OverrideData>();
